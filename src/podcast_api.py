a332c7d2bbc782b66ad920273901bf8ff8254ad0
Patrick Tran <patrick.tran06@gmail.com>
2023-05-18 11:30:47 -0700

    add cache

--- src/podcast_api.py
+++ src/podcast_api.py
@@ -1,6 +1,7 @@
 import os
 from loguru import logger
 from lcserve import serving
+import pickle
 from langchain_utils import get_summary, get_qa_with_sources, get_in_context_search
 from data_utils import get_youtube_transcript, read_data_pickle
 from typing import Dict
@@ -9,23 +10,31 @@ allin_youtube_episodes_df = read_data_pickle('../data/allin_youtube_episodes_df.
 allin_faiss_index = read_data_pickle('../data/allin_faiss_index.pkl')
 
 
+cache = read_data_pickle("../data/summary_cache.pkl")
+
 @serving
 def get_summarized_topics(videoid:str, **kwargs) -> str:
+    if videoid in cache:
+        return cache[videoid]
+
     transcript = get_youtube_transcript(videoid)
     logger.info(f"Transcript: {transcript[:100]}...")
     if transcript:
         topic_summary = get_summary(transcript)
+        cache[videoid] = topic_summary
+        with open("../data/summary_cache.pkl", "wb") as f:
+            pickle.dump(cache, f)
         return topic_summary
     else:
         return ''
-    
+
 
 @serving
 def get_qa_search(querytext:str, **kwargs) -> Dict:
     answer = get_qa_with_sources(querytext, allin_faiss_index)
-    return answer 
+    return answer
 
 
 def get_context_search(timestamp: float, videoid:str,  **kwargs) -> Dict:
     answer = get_in_context_search(timestamp, videoid, allin_youtube_episodes_df, allin_faiss_index)
-    return answer 
+    return answer
